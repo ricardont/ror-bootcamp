c9b7d6d63af2be16e76e139b7c70a93c835c1ac8
Ricky <rontiverosmarquez@gmail.com>
2018-12-17 23:00:57 -0700

    initial pizzaburger app

---
+++ pizzaburger.rb
@@ -0,0 +1,24 @@
+
+
+class PizzaBurger
+  def init()
+  end
+
+  def order(name='Jon Doe', phone='666-666-66')
+  	@name = name;
+  	@phone = phone;
+     "Hi #{@name}, your phone is #{@phone}}"
+	end
+	def menu
+		print """ 
+			Welcome to PizzaBurger,
+      What would you like to do?
+      	1. Order a pizza
+      	2. Order a burger
+      	3. List all orders
+				4. Cancel an order
+				0. Exit
+		"""
+	end
+end
+
---
+++ spec/pizzaburger_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../pizzaburger.rb'
+RSpec.describe PizzaBurger do 
+	it 'shows menu' do
+		pending
+		app = PizzaBurger.new
+
+	end	
+	it 'orders a pizza succefully' do
+		app = PizzaBurger.new;
+		#pizza =  Pizza.new;
+		order = app.order('Bob', '7-52-45-87' )
+		expect(order).to eq("Hi Bob, your phone is 7-52-45-87" );
+	end
+end
